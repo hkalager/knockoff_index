76f928be5221fb31d5cca45897fa24a43be03820
Arman Hassanniakalager <hassannia@outlook.com>
2023-01-05 18:10:02 +0000

    Dropped MATLAB scripts

--- Legacy MATLAB scripts/init_knockoffs.m
+++
@@ -1,9 +0,0 @@
-addpath(genpath(pwd));
-
-try
-    [S]=knockoffs.filter(rand(500,5), rand(500,1),.1,{'fixed'} );
-catch
-    warning(' CVX package not found; installing the package ... no admin access required!');
-    cvx_setup;
-    
-end
